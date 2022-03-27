57bef6dca5f924521b26d2c320b6876cc555e77e
Valeria Tyan <vlrtyan@yandex.ru>
2022-03-27 21:09:32 +0300

    setting

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = '3000';
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`On port ${PORT}`)
+})
