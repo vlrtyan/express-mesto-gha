bede380cadd61ac7aa75d8bdff4040a47d80ebe6
Valeria Tyan <vlrtyan@yandex.ru>
2022-04-10 15:50:12 +0300

    убрать лишний код

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-    useNewUrlParser: true
-  })
+mongoose.connect('mongodb://localhost:27017/mestodb')
 
 app.use((req, res, next) => {
   req.user = {
